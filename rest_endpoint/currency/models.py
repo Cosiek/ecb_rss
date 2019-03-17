65b28f3f500cf366a3b5be4be4c77f8132b202d6
cosiek <witold.majchrzak@21s.pl>
2019-03-17 17:32:00 +0100

    Add currency model

--- rest_endpoint/currency/models.py
+++ rest_endpoint/currency/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=3, unique=True, verbose_name="Nazwa")
+
+
+class ExchangeRate(models.Model):
+    currency = models.ForeignKey(Currency, related_name='rates',
+                                 on_delete=models.CASCADE)
+    value = models.DecimalField(decimal_places=4, max_digits=8,
+                                verbose_name="Wartość względem EUR")
+    value_time = models.DateTimeField(verbose_name="Stan na czas")
