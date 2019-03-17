5e4cdf45e3fdc8dfb91fd6e2fa55da4d7d4c6b2b
cosiek <witold.majchrzak@21s.pl>
2019-03-17 14:23:04 +0100

    Get a list of exchange reference rates RSS links to use

--- scraper.py
+++ scraper.py
@@ -2,8 +2,10 @@
 # encoding: utf-8
 
 import asyncio
+from urllib.parse import urlparse, urlunparse
 
 import aiohttp
+from pyquery import PyQuery as pq
 
 
 async def get_url_content(url, session):
@@ -14,13 +16,36 @@ async def get_url_content(url, session):
         return resp.status, ''
 
 
+def get_links(url, txt):
+    """
+    Get a list of exchange reference rates RSS links.
+    """
+    parsed = urlparse(url)
+
+    d = pq(txt)
+    hrefs = []
+    for html_element in d('.rss'):
+        href = pq(html_element).attr('href')
+        if href.startswith('/rss/fxref-'):
+            hrefs.append(urlunparse((parsed[0], parsed[1], href, "", "", "")))
+    return hrefs
+
+
+async def scrap(url, session):
+    status, txt = await get_url_content(url, session)
+    if status != 200:
+        # TODO: handle this somehow (maybe keep knocking)
+        return
+    links = get_links(url, txt)
+    print(links)
+
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     while True:
         session = aiohttp.ClientSession()
         # get feed data
         loop.run_until_complete(
-            get_url_content(
-                'https://www.ecb.europa.eu/home/html/rss.en.html', session)
+            scrap('https://www.ecb.europa.eu/home/html/rss.en.html', session)
         )
         loop.run_until_complete(session.close())
